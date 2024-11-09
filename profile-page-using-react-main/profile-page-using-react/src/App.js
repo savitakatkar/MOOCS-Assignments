63600c52876615abb9b38eb97da8f5a227eaa7f2
savitakatkar <Savitakatkar664@gmail.com>
2024-11-09 12:52:44 +0530

    Add files via upload

---
+++ profile-page-using-react-main/profile-page-using-react/src/App.js
@@ -0,0 +1,14 @@
+
+   // src/App.js
+import React from 'react';
+import ProfilePage from './components/ProfilePage';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfilePage />
+    </div>
+  );
+}
+
+export default App;
